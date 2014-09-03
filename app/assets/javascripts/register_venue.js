ed31192dbac2b1b50e3b71922fa6785c947cae99
Avi Fox-Rosen & David Bragdon <pair+avifoxi.postmodem@devbootcamp.com>
2014-09-02 21:07:06 -0400

    setup mvc format in js for register_venue.js form setup

--- app/assets/javascripts/register_venue.js
+++ app/assets/javascripts/register_venue.js
@@ -1,31 +1,50 @@
 
-var FormBuilder = function(els) {
+var VenueView = function(els) {
   this.addSpecButton = els.addSpecButton;
 }
 
-FormBuilder.prototype = {
+VenueView.prototype = {
+  init : function() {
+    this.addSpecFormListen();
+  },
   addSpecFormListen : function() {
     var _this = this;
     _this.addSpecButton.click(function(e) {
       e.preventDefault();
-      console.log(e);
-    })
-
+      // console.log(e);
+    });
   }
-
 }
 
 
+var VenueModel = function() {
+  this.name;
+  this.specs;
+}
 
+var VenueController = function(model, view) {
+  this.model = model;
+  this.view = view;
+
+}
+
+VenueController.prototype ={
+  init : function() {
+    this.view.init()
+  }
+}
 
 
 $(document).ready(function () {
 
-  formDude = new FormBuilder( {
+  vView = new VenueView( {
     addSpecButton : $('.addSpec')
   } );
 
-  formDude.addSpecFormListen();
+  vModel = new VenueModel();
 
+  vController = new VenueController(vModel, vView);
+
+  vController.init();
 })
 
