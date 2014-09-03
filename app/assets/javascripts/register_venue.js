d4c504852310f17342870556ce6f6f05f29c2e6e
Avi Fox-Rosen & David Bragdon <pair+avifoxi.postmodem@devbootcamp.com>
2014-09-02 22:45:44 -0400

    verbose draft 1 of mvc append spec div to form, TODO: refactor logic out of view

--- app/assets/javascripts/register_venue.js
+++ app/assets/javascripts/register_venue.js
@@ -13,11 +13,79 @@ VenueView.prototype = {
       e.preventDefault();
       // console.log(e);
     });
+  },
+  addNewSpecFields : function(specNum, fieldsObj) {
+
+    var _this = this;
+    var names = Object.getOwnPropertyNames(fieldsObj);
+
+    var specDiv = document.createElement('div');
+    var id = 'spec' + specNum;
+    specDiv.id = id;
+
+    for (var i = 0; i < names.length; i++){
+      var paramsKey = id + '[' + names[i] + ']'
+      var label = _this.formatLabel( paramsKey, names[i] );
+      var field = _this.prepField( paramsKey, fieldsObj[names[i]] );
+      specDiv.appendChild(label)
+      specDiv.appendChild(field)
+    }
+    $('.specsContainer').append(specDiv);
+  },
+  formatLabel : function(paramsKey, labelText){
+    var label = document.createElement('label');
+    label.setAttribute( "for", paramsKey );
+    label.innerText = labelText ;
+    return label;
+    // working
+  },
+  prepField : function(paramsKey, type) {
+    var _this = this;
+
+    switch(type) {
+      case 'string' :
+        return _this.formatTextIp(paramsKey);
+
+
+      case 'textarea' :
+        return _this.formatTextArea(paramsKey);
+
+      case 'boolean' :
+        return _this.formatBoolean(paramsKey);
+
+    }
+  },
+  //  works
+  formatTextIp : function(paramsKey){
+    var ip = document.createElement('input');
+    ip.type = 'text';
+    ip.name = paramsKey;
+    return ip;
+  },
+
+  formatTextArea : function(paramsKey) {
+    var ta = document.createElement('textarea');
+    ta.name = paramsKey;
+    return ta;
+  },
+
+  formatBoolean : function(paramsKey) {
+    var tf = ['true', 'false']
+    var sel = document.createElement('select');
+    sel.name = paramsKey;
+    for (var i=0; i<tf.length; i++) {
+      var opt = document.createElement('option');
+      opt.value = tf[i];
+      opt.innerText = tf[i];
+      sel.appendChild(opt)
+    }
+    return sel
   }
 }
 
 
-var VenueModel = function() {
+var VenueModel = function(spec_fields) {
+  this.spec_fields = spec_fields;
   this.name;
   this.specs;
 }
@@ -41,7 +109,13 @@ $(document).ready(function () {
     addSpecButton : $('.addSpec')
   } );
 
-  vModel = new VenueModel();
+   spec_fields = {
+    'equipment_type' : 'string',
+    'description' : 'textarea',
+    'functional' : 'boolean'
+  }
+
+  vModel = new VenueModel(spec_fields);
 
   vController = new VenueController(vModel, vView);
 
